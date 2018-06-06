65db34adef59ff41b05cc3bfff71020c91f3d00c
Chidimma Uba <chidimmajuba@gmail.com>
2018-06-06 15:15:19 -0400

    message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,41 +8,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random generate = new Random();
-
+        HashMap<String, String> map = new HashMap<String, String>();
         String[] qualifier = {"Why do you say that ", "You seem to think that ", "So, you are concerned that "};
         String[] hedges = {"Please tell me more ", "Many of my patients tell me the same thing ", "It is getting late, maybe we had better quit "};
         String output = "";
         boolean quit = false;
         System.out.println("How are you feeling?");
         String input = scan.nextLine();
-
-        //prompt for more questions
         do {
-            // move it outside the loop
-
-
             String[] a = input.split(" ");
+            map.put("I", "you");
+            map.put("me", "you");
+            map.put("my", "your");
+            map.put("am", "are");
             for (int i = 0; i < a.length; i++) {
-                if (a[i].equalsIgnoreCase("me") || a[i].equalsIgnoreCase("i")) {
-                    a[i] = "you";
-                } else if (a[i].equalsIgnoreCase("my")) {
-                    a[i] = "your";
-                } else if (a[i].equalsIgnoreCase("am")) {
-                    a[i] = "are";
+                for (String key : map.keySet()) {
+                    if (a[i].equalsIgnoreCase(key)) {
+                        a[i] = map.get(key);
+                    }
                 }
                 output += a[i] + " ";
             }
-
             int qualiferIndex = generate.nextInt(qualifier.length);
             int hedgeIndex = generate.nextInt(hedges.length);
-
-            String q = qualifier[qualiferIndex] +output + "?" ;
+            String q = qualifier[qualiferIndex] + output + "?";
             String h = hedges[hedgeIndex];
             String[] m = {q, h};
-
-
             System.out.println(m[generate.nextInt(m.length)]);
-
             System.out.println("Enter your response here or Q to quit");
             // use same name as the initial variable
             input = scan.nextLine();
@@ -54,4 +46,3 @@ public class Main {
 
     }
 }
-
